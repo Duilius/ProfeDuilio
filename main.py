5a39c68afbcd1abe911cf66b1a4f3427ab3b633e
Duilio Restuccia <restucciaduilio@gmail.com>
2024-05-27 23:22:29 -0500

    Trigonometria YouTube

--- main.py
+++ main.py
@@ -31,9 +31,54 @@ async def inicio(request:Request):
 ##########
 
 @app.get("/primaria", response_class=HTMLResponse)
-async def primaria(request:Request):
+async def cursos(request:Request):
     return templates.TemplateResponse("html/primaria.html", {"request":request})
 
+@app.get("/primaria/curso")
+async def primaria2(request: Request, el_curso: str):
+    print("El Curso es ====> ", el_curso)
+
+    if el_curso == "ari":
+        grado = "grados_aritmetica"
+    if el_curso == "alg":
+        grado = "grados_algebra"
+    if el_curso == "geo":
+        grado = "grados_geometria"
+    if el_curso == "tri":
+        grado = "grados_trigonometria"
+    if el_curso == "raz":
+        grado = "grados_raz_mat"
+    if el_curso == "est":        
+        grado = "grados_estadistica"
+
+    #print("El Curso es ====> ", grado)
+    return templates.TemplateResponse("html/"+ grado +".html", {"request":request})
+
+
+@app.get("/primaria/teoria-por-grado")
+async def primaria2(request: Request, curso: str, nivel:str, grado:str):
+    print("El Curso es ====> ", curso)
+    print("El Nivel es ====> ", nivel)
+
+    #/primaria/teoria-por-grado?curso=ari&grado=cinco&nivel=primaria
+
+    if grado == "cinco":
+        grado="5"
+        pagina = nivel+"_"+curso+"_"+grado+".html"
+    if grado == "sexto":
+        grado="6"
+    if grado == "primero":
+        grado="1"
+    if grado == "segundo":
+        grado="2"
+    if grado == "tercero":
+        grado="3"
+    if grado == "cuarto":
+        grado="4"
+    if grado == "quinto":
+        grado="5"        
+    #print("El Curso es ====> ", temario)
+    return templates.TemplateResponse("html/"+ nivel +"_"+ curso +"_" + grado +".html", {"request":request})
 
 ############
 # SECUNDARIA
@@ -51,6 +96,13 @@ async def primaria(request:Request):
 @app.get("/preuniversitaria", response_class=HTMLResponse)
 async def preuniversitaria(request:Request):
     return templates.TemplateResponse("html/preuniversitaria.html", {"request":request})
+
+##############
+#TRIGONOMETRÍA
+##############
+@app.get("/trigonometria", response_class=HTMLResponse)
+async def trigonometria(request:Request):
+    return templates.TemplateResponse("html/trigonometria.html", {"request":request})
 
 
 ##################
